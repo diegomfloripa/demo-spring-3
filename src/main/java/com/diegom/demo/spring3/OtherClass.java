974ad8008f9e89640fedafa5c9a03aca6a17970c
Diego Melo <diegomelodev@gmail.com>
2022-05-13 11:38:16 -0300

    add more issues

--- src/main/java/com/diegom/demo/spring3/OtherClass.java
+++ src/main/java/com/diegom/demo/spring3/OtherClass.java
@@ -1,8 +1,26 @@
 package com.diegom.demo.spring3;
 
+import org.slf4j.LoggerFactory;
+
 public class OtherClass {
 
-    public boolean isValid(int value) {
+    public boolean isValid(Integer value) {
+        if(value == null) {
+            LoggerFactory.getLogger(this.getClass()).error("valeu is null");
+            throw new RuntimeException("valeu is null");
+        }
         return value > 0;
+    }
+
+    public boolean isInvalid(Integer value) {
+        if(value == null) {
+            LoggerFactory.getLogger(this.getClass()).error("valeu is null");
+            throw new RuntimeException("valeu is null");
+        }
+        return value == 0;
+    }
+
+    public boolean isNull(Integer value) {
+        return value == null;
     }
 }
