cf35040428b3d1348a8349928729cf02737b2a2e
Diego Melo <diegomelodev@gmail.com>
2022-05-13 11:00:48 -0300

    first commit

---
+++ src/main/java/com/diegom/demo/spring3/AnyClass.java
@@ -0,0 +1,12 @@
+package com.diegom.demo.spring3;
+
+public class AnyClass {
+
+    public void testSonar() {
+        String val = null;
+
+        if(val.contains("a")) {
+            System.out.println("a");
+        }
+    }
+}
---
+++ src/test/java/com/diegom/demo/spring3/DemoSpring3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegom.demo.spring3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpring3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
